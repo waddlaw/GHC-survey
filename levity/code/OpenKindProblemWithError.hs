1a81fc6348abbbc5673c758e84d17b4ae7562d2a
waddlaw <ingronze@gmail.com>
2017-09-06 16:32:25 +0900

    トップレベルに unlifted type を定義した例を追加

---
+++ levity/code/OpenKindProblemWithError.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE MagicHash, ExplicitForAll, PolyKinds #-}
+
+import GHC.Prim (Int#, (+#))
+import GHC.Types (Int(I#))
+
+liftedFunc :: Int -> Int -> Int
+liftedFunc x y
+  | x < y = x + y
+  | otherwise = myError "x < y"
+
+unliftedFunc :: Int# -> Int# -> Int#
+unliftedFunc x y
+  | (I# x) < (I# y) = x +# y
+  | otherwise = myError "x < y"
+
+myError s = error ("Error: " ++ s)
+
+unliftedValue :: Int#
+unliftedValue = 0#
+
+main :: IO ()
+main = do
+  print $ liftedFunc 1 2
+  print $ I# (unliftedFunc 1# 2#)
+  print $ I# (unliftedFunc 2# 1#) -- error
+  print $ liftedFunc 2 1 -- error
